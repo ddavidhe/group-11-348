1e04b40dfede56fe99ed719084fae2839b3b896e
ruiichen <frankchen99999@gmail.com>
2025-10-17 14:24:28 -0400

    -  finished feature 1

--- milestone-1/queries/feature-1/tests/test_sample4.sql
+++ milestone-1/queries/feature-1/tests/test_sample4.sql
@@ -1,5 +1,5 @@
 SELECT firstName, lastName, driverTag, SUM(points.number) AS points
     FROM (results NATURAL JOIN races JOIN points ON results.finishPos = points.position AND races.season = points.season)
         RIGHT OUTER JOIN drivers ON drivers.dID = results.dID
-    WHERE round >= {} AND round <= {}
+    WHERE round >= 1 AND round <= 22 AND races.season = 2026
     GROUP BY drivers.dID, firstName, lastName, driverTag ORDER BY points DESC;
